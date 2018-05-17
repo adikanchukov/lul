d2638bf833bf21ac792fd5d9cb2289d7a53517d3
Alexander Dikanchukov <aleksandr.dikanchukov@iksatprof.ru>
2018-05-17 09:29:21 +0300

    bind: revise

--- bind.lua
+++ bind.lua
@@ -1,4 +1,23 @@
-function bind(func, ...)
-  local args = { ... }
-  return function() return func(table.unpack(args)) end
+function bind(func, arg)
+  return function() return func(arg) end
+end
+
+
+function bind2(func, args)
+  return function() return func(args[1], args[2]) end
+end
+
+
+function bind3(func, args)
+  return function() return func(args[1], args[2], args[3]) end
+end
+
+
+function bind4(func, args)
+  return function() return func(args[1], args[2], args[3], args[4]) end
+end
+
+
+function bind5(func, args)
+  return function() return func(args[1], args[2], args[3], args[4], args[5]) end
 end
--- test/test_bind.lua
+++ test/test_bind.lua
@@ -6,10 +6,42 @@ require 'bind'
 
 
 test.new('bind', 'bind', function(self)
-  local h = function(x, y) return x + y end
-  local f = bind(h, 3, 5)
+  local h = function(a) return a * a end
+  local f = bind(h, 3)
+
+  self.eq(f(), 9)
+end)
+
+
+test.new('bind', 'bind2', function(self)
+  local h = function(a, b) return a + b end
+  local f = bind2(h, { 3, 2 })
+
+  self.eq(f(), 5)
+end)
+
+
+test.new('bind', 'bind3', function(self)
+  local h = function(a, b, c) return a + b + c end
+  local f = bind3(h, { 3, 2, 3 })
 
   self.eq(f(), 8)
+end)
+
+
+test.new('bind', 'bind4', function(self)
+  local h = function(a, b, c, d) return a + b + c + d end
+  local f = bind4(h, { 3, 2, 3, 5 })
+
+  self.eq(f(), 13)
+end)
+
+
+test.new('bind', 'bind5', function(self)
+  local h = function(a, b, c, d, e) return a + b + c + d + e end
+  local f = bind5(h, { 3, 2, 3, 5, 5 })
+
+  self.eq(f(), 18)
 end)
 
 
